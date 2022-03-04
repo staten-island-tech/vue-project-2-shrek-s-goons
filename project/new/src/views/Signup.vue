0a0e82365ac4565a086bdc1b484855ece3781758
JasonLiao118 <90655376+JasonLiao118@users.noreply.github.com>
2022-03-04 10:53:55 -0500

    asQ

--- project/new/src/views/Signup.vue
+++ project/new/src/views/Signup.vue
@@ -1,5 +1,7 @@
 <template>
   <form>
+    <h1>Sign Up</h1>
+    <input type="email" name="email" v-model="email" required />
     <label for="email">Password:</label>
     <input type="password" name="password" v-model="password" required />
     <button>Sign up</button>
@@ -7,7 +9,24 @@
 </template>
 
 <script>
-export default {};
+import { useStore } from "vuex";
+export default {
+  setup() {
+    const email = ref("");
+    const password = ref("");
+
+    const store = useStore();
+
+    const handleSubmit = () => {
+      store.dispatch("signup", {
+        email: email.value,
+        password: password.value,
+      });
+    };
+
+    return { handleSubmit, email, password };
+  },
+};
 </script>
 
 <style></style>

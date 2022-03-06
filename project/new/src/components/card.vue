214fed3675e9c97828f55e1b139f5f73a99d02cd
JasonLiao118 <jack666280@gmail.com>
2022-03-06 13:53:40 -0500

    sdawdsad

---
+++ project/new/src/components/card.vue
@@ -0,0 +1,65 @@
+<template>
+  <div class="card">
+    <h2>{{ title }}</h2>
+    <img class="cardImage" :src="getImage" alt="" />
+    <h3>${{ price }}</h3>
+    <h4>Tier={{ tier }}</h4>
+    <Button v-if="user" @button-click="$emit('add')">Add to Cart</Button>
+  </div>
+</template>
+
+<script>
+import Button from "../components/button.vue";
+import useStore from "vuex";
+import { computed } from "vuex";
+
+export default {
+  name: "Card",
+  components: {
+    Button: Button,
+  },
+  setup() {
+    const store = useStore();
+    return {
+      user: computed(() => store.state.user),
+    };
+  },
+  props: {
+    title: String,
+    price: Number,
+    image: String,
+    tier: String,
+  },
+  computed: {
+    getImage: function () {
+      return this.image;
+    },
+  },
+};
+</script>
+
+<style>
+.card {
+  width: 25%;
+  height: 300px;
+  background-color: rgb(9, 65, 9);
+  border: greenyellow 5px solid;
+  text-align: center;
+  padding: 15px;
+  flex-direction: crow;
+  justify-content: space-around;
+  box-shadow: 0 1.5rem 4rem rgb(0, 0, 0);
+  flex: wrap;
+  flex-wrap: wrap;
+  margin-top: 80px;
+  border-radius: 50px;
+}
+.cardImage {
+  max-width: 60px;
+  object-fit: cover;
+}
+
+h1 {
+  color: white;
+}
+</style>
--- project/new/src/views/Home.vue
+++ project/new/src/views/Home.vue
@@ -1,40 +1,132 @@
 <template>
   <div class="home">
-    <div v-for="blog in blogs" :key="blog.id">
-      <div class="blog">
-        <h3>{{ blog.title }}</h3>
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Pariatur
-          aspernatur consectetur doloremque sunt ducimus enim iure animi fugit
-          nulla et! Perferendis autem deleniti quo eum corrupti reiciendis
-          voluptatem ab ducimus?
-        </p>
-        <div class="icons" v-if="user">
-          <span>upvote or downvote this article: </span>
-          <span class="material-icons">thumb_up</span>
-          <span class="material-icons">thumb_down</span>
-        </div>
-      </div>
-    </div>
+    <Card
+      v-for="(characters, index) in shrekCharacters"
+      :key="index"
+      :title="characters.name"
+      :price="characters.price"
+      :tier="characters.tier"
+      :image="characters.image"
+      @add="updateList(index)"
+    />
   </div>
 </template>
 
 <script>
 import { computed, ref } from "vue";
 import { useStore } from "vuex";
-export default {
-  setup() {
-    const blogs = ref([
-      { title: "Why Coffee is Better than Tea", id: 1 },
-      { title: "...Then I Took an Arrow in the Knee", id: 2 },
-      { title: "Mario vs Luigi, Ultimate Showdown", id: 3 },
-    ]);
-    const store = useStore();
+import Card from "../components/card.vue";
 
+export default {
+  components: {
+    Card,
+  },
+  setup() {
+    const store = useStore();
+    const shrekCharacters = ref([
+      {
+        name: "Shrek 1",
+        price: 1001,
+        tier: "S",
+        image:
+          "https://i.pinimg.com/474x/31/ba/98/31ba98cbc56e2b40060d3951dec3adda.jpg",
+      },
+      {
+        name: "Shrek 2",
+        price: 400,
+        tier: "S",
+        image:
+          "https://m.media-amazon.com/images/M/MV5BZmU5ZDE5NTItN2I1YS00ZmFmLTk3YTgtNzQwOGNkYzFjOWRkXkEyXkFqcGdeQXVyNzU1NzE3NTg@._V1_QL75_UX500_CR0,47,500,281_.jpg",
+      },
+      {
+        name: "Shrek 3",
+        price: 400,
+        tier: "S",
+        image:
+          "https://media.sketchfab.com/models/15753cc5826d4a94830309cf5c8c290d/thumbnails/e99812c5d5844413a32861469ca24f21/b72b6385ce704c63b8417ea922cfe594.jpeg",
+      },
+      {
+        name: "Fiona 1",
+        price: 300,
+        tier: "A",
+        image:
+          "https://people.southwestern.edu/~bednarb/su_netWorks/projects/mcentire/fiona.jpg",
+      },
+      {
+        name: "Fiona 2",
+        price: 300,
+        tier: "A",
+        image:
+          "https://preview.redd.it/3r3ut78wo4u51.jpg?auto=webp&s=188b221d64b3f5706e84f8d616ffe4b23435692e",
+      },
+      {
+        name: "Fiona 3",
+        price: 300,
+        tier: "A",
+        image:
+          "https://i.pinimg.com/236x/6d/0f/ce/6d0fce5676847efb72456ece6b0587aa--shrek-plans.jpg",
+      },
+      {
+        name: "Donkey 1",
+        price: 200,
+        tier: "B",
+        image:
+          "https://i.pinimg.com/originals/70/d4/d4/70d4d4ff305cf54a08a4689bbe778405.jpg",
+      },
+      {
+        name: "Donkey 2",
+        price: 200,
+        tier: "B",
+        image:
+          "https://img.huffingtonpost.com/asset/586d61191500009206e9e7b2.png?cache=gOeX9kAIdl&ops=1778_1000",
+      },
+      {
+        name: "Donkey 3",
+        price: 200,
+        tier: "B",
+        image: "https://img.fruugo.com/product/4/52/108501524_max.jpg",
+      },
+    ]);
     return {
-      blogs,
+      shrekCharacters,
       user: computed(() => store.state.user),
     };
+  },
+  data: function () {
+    return {
+      order: [],
+      orderPrice: [],
+      subtotal: 0,
+    };
+  },
+  methods: {
+    updateList(index) {
+      this.order.push(this.shrekCharacters[index].name);
+      this.orderPrice.push(this.shrekCharacters[index].price);
+      this.subtotal = this.subtotal + this.shrekCharacters[index].price;
+      if (this.subtotal > 1000) {
+        this.order = [];
+        this.orderPrice = [];
+        this.subtotal = 0;
+        this.$router.push("about");
+      }
+    },
+    removeLastItem: function () {
+      if (this.order.length !== 0) {
+        this.subtotal =
+          this.subtotal - this.orderPrice[this.orderPrice.length - 1];
+        this.order.pop();
+        this.orderPrice.pop();
+      }
+    },
+    removeAllItems: function () {
+      this.order = [];
+      this.orderPrice = [];
+      this.subtotal = 0;
+    },
+    checkout: function () {
+      this.$router.push("Cart");
+    },
   },
 };
 </script>

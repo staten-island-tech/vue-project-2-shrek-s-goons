cc3c363c84303949ea03047804b0348c6d460e0f
JasonLiao118 <90655376+JasonLiao118@users.noreply.github.com>
2022-03-24 10:38:27 -0400

    wdasd

---
+++ project/new/src/components/Clicker.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <p><button v-on:click="counter += 1">Add One More Click</button></p>
+    <p>The button has been clicked {{ counter }} times</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "clicker",
+  data: function () {
+    return {
+      counter: 0,
+    };
+  },
+};
+</script>
--- project/new/src/views/Home.vue
+++ project/new/src/views/Home.vue
@@ -1,6 +1,30 @@
 <template>
+<<<<<<< Updated upstream
   <div>
     <Button @button-click="add">Yes</Button>
+=======
+  <div class="mainOrder">
+    <Clicker></Clicker>
+
+    <Card
+      v-for="(characters, index) in shrekCharacters"
+      :key="index"
+      :title="characters.name"
+      :price="characters.price"
+      :tier="characters.tier"
+      :image="characters.image"
+      @add="updateList(index)"
+    />
+
+    <Cart
+      :subtotal="this.subtotal"
+      :order="this.order"
+      :orderPrice="this.orderPrice"
+      @removeAll="removeAllItems"
+      @undo="removeLastItem"
+      @checkout="checkout"
+    />
+>>>>>>> Stashed changes
   </div>
 </template>
 

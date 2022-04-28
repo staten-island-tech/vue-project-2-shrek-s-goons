2865b5b2f31023c338c91d2057271e856befcf42
Theodore-Fan <Theodorefan17@gmail.com>
2022-04-28 10:46:54 -0400

    sbsrbpnge

--- project/new/src/components/Clicker.vue
+++ project/new/src/components/Clicker.vue
@@ -1,7 +1,7 @@
 <template>
-
   <button @click="updatePoints(5), audio()" class="BTN">
-  <img class="ROCK"
+    <img
+      class="ROCK"
       src="https://res.cloudinary.com/teepublic/image/private/s--yCiRU3FK--/c_crop,x_10,y_10/c_fit,w_830/c_crop,g_north_west,h_1038,w_1038,x_-104,y_-128/l_upload:v1565806151:production:blanks:vdbwo35fw6qtflw9kezw/fl_layer_apply,g_north_west,x_-215,y_-239/b_rgb:000000/c_limit,f_jpg,h_630,q_90,w_630/v1588017884/production/designs/9553847_0.jpg"
     />
   </button>
@@ -14,9 +14,9 @@ export default {
     updatePoints(points) {
       this.$store.commit("updatePoints", points);
     },
-    audio(){
+    audio() {
       console.log("YO");
-    }
+    },
   },
   computed: {
     points() {
@@ -26,7 +26,7 @@ export default {
 };
 </script>
 <style>
-.ROCK{
+.ROCK {
   height: 500px;
 }
 .BTN {

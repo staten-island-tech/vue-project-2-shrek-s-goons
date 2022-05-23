d6a076782ea53c17351e1caa419e6dc66c02b0e9
JasonLiao118 <90655376+JasonLiao118@users.noreply.github.com>
2022-05-23 10:37:26 -0400

    wdasd

--- project/new/src/components/Clicker.vue
+++ project/new/src/components/Clicker.vue
@@ -1,5 +1,5 @@
 <template>
-  <button @click="updatePoints(this.additiveValue)" class="BTN">
+  <button @click="updatePoints(this.additiveValue) && checkWin()" class="BTN">
     <img
       class="ROCK"
       src="https://res.cloudinary.com/teepublic/image/private/s--yCiRU3FK--/c_crop,x_10,y_10/c_fit,w_830/c_crop,g_north_west,h_1038,w_1038,x_-104,y_-128/l_upload:v1565806151:production:blanks:vdbwo35fw6qtflw9kezw/fl_layer_apply,g_north_west,x_-215,y_-239/b_rgb:000000/c_limit,f_jpg,h_630,q_90,w_630/v1588017884/production/designs/9553847_0.jpg"
@@ -23,6 +23,11 @@ export default {
     };
   },
   methods: {
+    checkWin(){
+      if(this.points>1000000000){
+        this.$route.
+      }
+    },
     updatePoints(points) {
       if (this.user) {
         this.$store.commit("updatePoints", points);

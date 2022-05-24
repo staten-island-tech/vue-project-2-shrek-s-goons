47dc9d49d11514c1a7fa30ad76106b9a4e82b0a7
Theodore-Fan <Theodorefan17@gmail.com>
2022-05-23 21:17:35 -0400

    aspojafwag

--- project/new/src/components/Clicker.vue
+++ project/new/src/components/Clicker.vue
@@ -1,5 +1,5 @@
 <template>
-  <button @click="updatePoints(5), audio()" class="BTN">
+  <button @click="updatePoints(5), checkWin()" class="BTN">
     <img
       class="ROCK"
       src="https://res.cloudinary.com/teepublic/image/private/s--yCiRU3FK--/c_crop,x_10,y_10/c_fit,w_830/c_crop,g_north_west,h_1038,w_1038,x_-104,y_-128/l_upload:v1565806151:production:blanks:vdbwo35fw6qtflw9kezw/fl_layer_apply,g_north_west,x_-215,y_-239/b_rgb:000000/c_limit,f_jpg,h_630,q_90,w_630/v1588017884/production/designs/9553847_0.jpg"
@@ -12,15 +12,11 @@ export default {
   name: "Clicker",
   methods: {
     updatePoints(points) {
-      this.$store.commit("updatePoints", points);
-    },
-    audio() {
-      console.log("YO");
-    },
-  },
-  computed: {
-    points() {
-      return this.$store.state.points;
+      if (this.user) {
+        this.$store.commit("updatePoints", points);
+      } else {
+        alert("Shrek Says Log In First");
+      }
     },
   },
 };
